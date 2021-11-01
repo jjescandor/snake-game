84232262f05784bf8f443c9755e0f88bb02783eb
JJ Escandor <jj.p.escandor@gmail.com>
2021-11-01 18:34:52 -0400

    added snake movement functionality

--- public/js/Snake.js
+++ public/js/Snake.js
@@ -1,10 +1,12 @@
 export default class Snake {
     constructor(scene) {
         this.scene = scene;
-        this.direction = Phaser.Math.Vector2.RIGHT;
+        this.lastMoveTime = 0;
+        this.moveInterval = 500;
+        this.direction = Phaser.Math.Vector2.DOWN;
         this.body = [];
-        this.body.push(this.scene.add.rectangle(10, 10, 16, 16, 0xff0000).setOrigin(0));
-        this.body.push(this.scene.add.rectangle(10, 10, 16, 16, 0x0000ff).setOrigin(0));
+        this.body.push(this.scene.add.rectangle(0, 0, 16, 16, 0xff0000).setOrigin(0));
+        this.body.push(this.scene.add.rectangle(0, 0, 16, 16, 0x0000ff).setOrigin(0));
         scene.input.keyboard.on('keydown', e => {
             this.keydown(e);
         });
@@ -13,19 +15,28 @@ export default class Snake {
     keydown(event) {
         switch (event.keyCode) {
             case 37: // left
+                this.direction = Phaser.Math.Vector2.LEFT;
                 break;
             case 38: // up
+                this.direction = Phaser.Math.Vector2.UP;
                 break;
             case 39: // right
+                this.direction = Phaser.Math.Vector2.RIGHT;
                 break;
             case 40: // down
+                this.direction = Phaser.Math.Vector2.DOWN;
                 break;
         }
     }
     update(time) {
-        this.body[0].x += this.direction.x;
-        this.body[0].y += this.direction.y;
-
+        if (time >= this.lastMoveTime + this.moveInterval) {
+            this.lastMoveTime = time;
+            this.move();
+        }
+    }
+    move() {
+        this.body[0].x += this.direction.x * 16;
+        this.body[0].y += this.direction.y * 16;
 
     }
 }
